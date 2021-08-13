d11c4665c7439866db609a1140f7ad3fcea83c45
franklinschool <bryan.mulligan@gmail.com>
2021-08-12 21:04:31 -0500

    added tailwind and alpinejs

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # birthday
+
+A project to blow out the candles
+
+Using
+- TailwindCSS
+- AlpineJS
---
+++ public/js/app.js
@@ -0,0 +1,6 @@
+// First ask for how many years they will be.
+function howOld() {
+   let age = prompt("How old are you going to be?", "Enter your age");
+   document.write("You have entered : " + age);
+}
+// howOld()
